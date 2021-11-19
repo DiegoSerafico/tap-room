ec38b1ca2b5e04d81cec92c30551482360599769
Diego Serafico <dseraficohernandez@gmail.com>
2021-11-19 10:46:14 -0800

    add control with keg list

---
+++ tap-room/src/components/TapRoomControl.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class TapRoomControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listVisible: true,
+      formVisible: false,
+      selectedKeg: null,
+      kegList= [
+        {
+          name: 'Corona Extra',
+          brand: 'Cerveceria Modelo',
+          price: 120,
+          pintPrice: 13,
+          alcoholContent: 4.6
+        },
+        {
+          name: 'Modelo Especial',
+          brand: 'Cerveceria Modelo',
+          price: 160,
+          pintPrice: 15,
+          alcoholContent: 4.4
+        },
+        {
+          name: 'Negra Modelo',
+          brand: 'Cerveceria Modelo',
+          price: 180,
+          pintPrice: 16,
+          alcoholContent: 5.4
+        },
+        {
+          name: 'Heineken',
+          brand: 'Heineken Company',
+          price: 280,
+          pintPrice: 16,
+          alcoholContent: 5
+        }
+      ]
+    }
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default TapRoomControl;
